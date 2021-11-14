00e92b4405356368f60b4f5965d16840c707994d
adam-lafontaine <adam.lafontaine@gmail.com>
2021-11-13 23:01:40 -0500

    cleanup palette

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -18,10 +18,10 @@ constexpr r64 MBT_MAX_Y = 1.2;
 constexpr r64 MBT_WIDTH = MBT_MAX_X - MBT_MIN_X;
 constexpr r64 MBT_HEIGHT = MBT_MAX_Y - MBT_MIN_Y;
 
-constexpr u32 NUM_COLORS = 511;
+constexpr u32 NUM_SHADES = 256;
 
 constexpr u32 MAX_ITERTAIONS_LOWER_LIMIT = 50;
-constexpr u32 MAX_ITERATIONS_UPPER_LIMIT = 10000;
+constexpr u32 MAX_ITERATIONS_UPPER_LIMIT = 50000;
 constexpr u32 MAX_ITERATIONS_START = MAX_ITERTAIONS_LOWER_LIMIT;
 
 class Point2Dr64
@@ -160,23 +160,22 @@ static void fill(image_t const& dst, pixel_t const& p)
 
 
 
-using color_palette_t = std::array<pixel_t, NUM_COLORS>;
+using gray_palette_t = std::array<u8, NUM_SHADES * 2 - 1>;
 
 
-constexpr color_palette_t make_gray_palette()
+constexpr gray_palette_t make_gray_palette()
 {
-	color_palette_t palette = {};
+	gray_palette_t palette = {};
 
-	for (u32 i = 0; i < 256; ++i)
+	for (u32 i = 0; i < NUM_SHADES; ++i)
 	{
-		u8 c = static_cast<u8>(i);
-		palette[i] = to_pixel(c, c, c);
+		palette[i] = static_cast<u8>(i);
 	}
 
-	for (u32 i = 256; i < NUM_COLORS; ++i)
+	for (u32 i = NUM_SHADES; i < palette.size(); ++i)
 	{
-		u8 c = static_cast<u8>(510 - i);
-		palette[i] = to_pixel(c, c, c);
+		u8 c = static_cast<u8>(palette.size() - 1 - i);
+		palette[i] = c;
 	}
 
 	return palette;
@@ -207,7 +206,25 @@ static pixel_t to_gray(r64 ratio)
 		g = n_colors - 1;
 	}
 
-	return palette[g];
+	auto const shade = palette[g];
+
+	return to_pixel(shade, shade, shade);
+}
+
+
+using color_palette_t = std::array<u8, NUM_SHADES>;
+
+
+constexpr color_palette_t make_color_palette()
+{
+	color_palette_t palette = {};
+
+	for (u32 i = 0; i < NUM_SHADES; ++i)
+	{
+		palette[i] = static_cast<u8>(i);
+	}
+
+	return palette;
 }
 
 
@@ -223,10 +240,10 @@ static pixel_t to_rgb(r64 ratio)
 	auto const q2 = q * q;
 	auto const q3 = q2 * q;
 
-	constexpr auto palette = make_gray_palette();
+	constexpr auto palette = make_color_palette();
 	constexpr auto n_colors = static_cast<u32>(palette.size());
 
-	auto rr = 16.0 * q2 * p2;
+	auto rr = 4 * q2 * p2;
 	auto r = static_cast<u32>(rr * (n_colors - 1));
 	if (r >= n_colors)
 	{
@@ -247,11 +264,11 @@ static pixel_t to_rgb(r64 ratio)
 		b = n_colors - 1;
 	}
 
-	auto const red = palette[r];
-	auto const green = palette[g];
-	auto const blue = palette[b];
+	u8 const red = palette[r];
+	u8 const green = palette[g];
+	u8 const blue = palette[b];
 
-	return to_platform_pixel(r, g, b);
+	return to_platform_pixel(red, green, blue);
 }
 
 
@@ -316,38 +333,14 @@ constexpr pixel_t to_rgb2(r64 ratio)
 }
 
 
-//using color_palette_t = std::array<pixel_t, MAX_ITERATIONS_START>;
-//
-//
-//constexpr color_palette_t make_rgb_palette()
-//{
-//	color_palette_t palette = {};
-//
-//	for (u32 i = 0; i < MAX_ITERATIONS_START; ++i)
-//	{
-//		auto ratio = static_cast<r64>(i) / (MAX_ITERATIONS_START);
-//		palette[i] = to_rgb(ratio);
-//	}
-//
-//	return palette;
-//}
-
-
-//constexpr pixel_t to_color(size_t index)
-//{
-//	constexpr auto palette = make_rgb_palette();
-//
-//	if (index >= palette.size())
-//	{
-//		index = palette.size() - 1;
-//	}
-//
-//	return palette[index];
-//}
-
-
-static void draw(image_t const& dst, mat_u32_t const& mat, u32 min, u32 max)
+static void draw(image_t const& dst, AppState const& state)
 {
+	auto& mat = state.iterations;
+
+	auto [mat_min, mat_max] = std::minmax_element(mat.begin(), mat.end());
+	auto const min = *mat_min;
+	auto const max = *mat_max;
+
 	auto const diff = static_cast<r64>(max - min);
 
 	auto const to_platform_color = [&](u32 i) 
@@ -382,25 +375,6 @@ static void for_each_row(mat_u32_t const& mat, std::function<void(u32 y)> const&
 }
 
 
-static void copy_left(image_t const& img, u32 n_cols)
-{
-	u32 const x_len = img.width - n_cols;
-
-	auto const copy_row_part = [&](u32 y)
-	{
-		auto row = img.row_begin(y);
-		for (u32 ix = 0; ix < x_len; ++ix)
-		{
-			auto src_x = ix + n_cols;
-			auto dst_x = src_x - n_cols;
-			row[dst_x] = row[src_x];
-		}
-	};
-
-	for_each_row(img, copy_row_part);
-}
-
-
 static void copy_left(mat_u32_t const& mat, u32 n_cols)
 {
 	u32 const x_len = mat.width - n_cols;
@@ -420,25 +394,6 @@ static void copy_left(mat_u32_t const& mat, u32 n_cols)
 }
 
 
-static void copy_right(image_t const& img, u32 n_cols)
-{
-	u32 const x_len = img.width - n_cols;
-
-	auto const copy_row_part = [&](u32 y)
-	{
-		auto row = img.row_begin(y);
-		for (u32 ix = 0; ix < x_len; ++ix)
-		{
-			auto src_x = x_len - 1 - ix;
-			auto dst_x = src_x + n_cols;
-			row[dst_x] = row[src_x];
-		}
-	};
-
-	for_each_row(img, copy_row_part);
-}
-
-
 static void copy_right(mat_u32_t const& mat, u32 n_cols)
 {
 	u32 const x_len = mat.width - n_cols;
@@ -455,67 +410,6 @@ static void copy_right(mat_u32_t const& mat, u32 n_cols)
 	};
 
 	for_each_row(mat, copy_row_part);
-}
-
-
-static void copy(image_t const& img, Vec2Di32 const& direction)
-{
-	auto up = direction.y < 0;
-	auto right = direction.x > 0;
-
-	auto const n_cols = static_cast<u32>(std::abs(direction.x));
-	auto const n_rows = static_cast<u32>(std::abs(direction.y));
-
-	if (n_cols == 0 && n_rows == 0)
-	{
-		return;
-	}
-
-	if (n_rows == 0)
-	{
-		if (right)
-		{
-			copy_right(img, n_cols);
-		}
-		else
-		{
-			copy_left(img, n_cols);
-		}
-
-		return;
-	}
-
-	u32 const x_len = img.width - n_cols;
-	u32 const y_len = img.height - n_rows;
-
-	u32 src_x_begin = right ? 0 : n_cols;
-	u32 dst_x_begin = src_x_begin + direction.x;
-
-	u32 src_y_begin = 0;
-	i32 dy = 0;
-	if (up)
-	{
-		src_y_begin = n_rows;
-		dy = 1;
-	}
-	else
-	{
-		src_y_begin = y_len - 1;
-		dy = -1;
-	}
-
-	for (u32 iy = 0; iy < y_len; ++iy)
-	{
-		auto src_y = src_y_begin + dy * iy;
-		auto dst_y = src_y + direction.y;
-
-		auto src_begin = img.row_begin(src_y) + src_x_begin;
-		auto dst_begin = img.row_begin(dst_y) + dst_x_begin;
-
-		auto src_end = src_begin + x_len;
-
-		std::copy(std::execution::par, src_begin, src_end, dst_begin);
-	}
 }
 
 
@@ -577,142 +471,6 @@ static void copy(mat_u32_t const& mat, Vec2Di32 const& direction)
 
 		std::copy(std::execution::par, src_begin, src_end, dst_begin);
 	}
-}
-
-
-static void mandelbrot(image_t const& dst, AppState const& state)
-{
-	auto const max_iter = state.max_iter;
-	constexpr r64 limit = 4.0;
-
-	auto const x_pos = state.screen_pos.x;
-	auto const y_pos = state.screen_pos.y;
-
-	auto const min_re = MBT_MIN_X + x_pos;
-	auto const min_im = MBT_MIN_Y + y_pos;
-
-	auto const re_step = screen_width(state) / dst.width;
-	auto const im_step = screen_height(state) / dst.height;
-
-	auto const do_mandelbrot = [&](Range2Du32 const& range)
-	{
-		auto x_ids = UnsignedRange(range.x_begin, range.x_end);
-		auto y_ids = UnsignedRange(range.y_begin, range.y_end);
-
-		auto y_id_begin = y_ids.begin();
-		auto y_id_end = y_ids.end();
-		auto x_id_begin = x_ids.begin();
-		auto x_id_end = x_ids.end();
-
-		auto const do_row = [&](u32 y)
-		{
-			r64 const ci = min_im + y * im_step;
-			auto row = dst.row_begin(y);
-
-			auto const do_x = [&](u32 x)
-			{
-				u32 iter = 0;
-				r64 const cr = min_re + x * re_step;
-
-				r64 re = 0.0;
-				r64 im = 0.0;
-				r64 re2 = 0.0;
-				r64 im2 = 0.0;
-
-				while (iter < max_iter && re2 + im2 <= limit)
-				{
-					im = (re + re) * im + ci;
-					re = re2 - im2 + cr;
-					im2 = im * im;
-					re2 = re * re;
-
-					++iter;
-				}
-
-				//row[x] = to_platform_pixel(to_color(iter - 1));
-			};
-
-			std::for_each(std::execution::par, x_id_begin, x_id_end, do_x);
-		};
-
-		std::for_each(y_id_begin, y_id_end, do_row);	
-	};
-
-	auto& shift = state.pixel_shift;
-
-	auto do_left = shift.x > 0;
-	auto do_top = shift.y > 0;
-	//auto do_right = shift.x < 0;	
-	//auto do_bottom = shift.y < 0;
-
-	auto const n_cols = static_cast<u32>(std::abs(shift.x));
-	auto const n_rows = static_cast<u32>(std::abs(shift.y));
-
-	auto no_horizontal = n_cols == 0;
-	auto no_vertical = n_rows == 0;
-
-	auto r = get_range(dst);
-
-	if (no_horizontal && no_vertical)
-	{
-		do_mandelbrot(r);
-		return;
-	}
-
-	if (no_horizontal)
-	{
-		if (do_top)
-		{
-			r.y_end = n_rows;
-		}
-		else // if (do_bottom)
-		{
-			r.y_begin = dst.height - 1 - n_rows;
-		}
-
-		do_mandelbrot(r);
-		return;
-	}
-
-	if (no_vertical)
-	{
-		if (do_left)
-		{
-			r.x_end = n_cols;
-		}
-		else // if (do_right)
-		{
-			r.x_begin = dst.width - 1 - n_cols;
-		}
-
-		do_mandelbrot(r);
-		return;
-	}
-
-	auto r2 = r;
-
-	if (do_top)
-	{
-		r.y_end = n_rows;
-		r2.y_begin = n_rows;
-	}
-	else // if (do_bottom)
-	{
-		r.y_begin = dst.height - 1 - n_rows;
-		r2.y_end = dst.height - 1 - n_rows;
-	}
-
-	if (do_left)
-	{
-		r2.x_end = n_cols;
-	}
-	else // if (do_right)
-	{
-		r2.x_begin = dst.width - 1 - n_cols;
-	}
-
-	do_mandelbrot(r);
-	do_mandelbrot(r2);
 }
 
 
@@ -856,27 +614,13 @@ static void mandelbrot(AppState& state)
 }
 
 
-//static void render(image_t const& dst, AppState& state)
-//{	
-//	bool old = false;
-//
-//	if (old)
-//	{
-//		copy(dst, state.pixel_shift);
-//		mandelbrot(dst, state);
-//	}
-//	else
-//	{
-//		copy(state.iterations, state.pixel_shift);
-//		mandelbrot(state);
-//
-//		draw(dst, state);
-//	}
-//}
+static void render(image_t const& dst, AppState& state)
+{	
+	copy(state.iterations, state.pixel_shift);
+	mandelbrot(state);
 
-
-
-
+	draw(dst, state);
+}
 
 
 namespace app
@@ -909,7 +653,53 @@ namespace app
 		r64 const zoom_per_second = 0.5;
 		r64 zoom = 1.0 + zoom_per_second * input.dt_frame;
 
-		if (input.keyboard.w_key.is_down)
+		auto direction = false;
+
+		if (input.keyboard.right_key.is_down)
+		{
+			auto distance_per_pixel = screen_width(state) / BUFFER_WIDTH;
+
+			state.pixel_shift.x -= pixel_shift;
+			state.screen_pos.x += pixel_shift * distance_per_pixel;
+
+			direction = true;
+			state.render_new = true;
+		}
+
+		if (input.keyboard.left_key.is_down)
+		{
+			auto distance_per_pixel = screen_width(state) / BUFFER_WIDTH;
+
+			state.pixel_shift.x += pixel_shift;
+			state.screen_pos.x -= pixel_shift * distance_per_pixel;
+
+			direction = true;
+			state.render_new = true;
+		}
+
+		if (input.keyboard.up_key.is_down)
+		{
+			auto distance_per_pixel = screen_height(state) / BUFFER_HEIGHT;
+
+			state.pixel_shift.y += pixel_shift;
+			state.screen_pos.y -= pixel_shift * distance_per_pixel;
+
+			direction = true;
+			state.render_new = true;
+		}
+
+		if (input.keyboard.down_key.is_down)
+		{
+			auto distance_per_pixel = screen_height(state) / BUFFER_HEIGHT;
+
+			state.pixel_shift.y -= pixel_shift;
+			state.screen_pos.y += pixel_shift * distance_per_pixel;
+
+			direction = true;
+			state.render_new = true;
+		}
+
+		if (input.keyboard.w_key.is_down && !direction)
 		{
 			auto old_w = screen_width(state);
 			auto old_h = screen_height(state);
@@ -917,13 +707,13 @@ namespace app
 			auto new_w = screen_width(state);
 			auto new_h = screen_height(state);
 
-			state.screen_pos.x += 0.5 * (old_w - new_w);	
+			state.screen_pos.x += 0.5 * (old_w - new_w);
 			state.screen_pos.y += 0.5 * (old_h - new_h);
 
 			state.render_new = true;
 		}
 
-		if (input.keyboard.s_key.is_down)
+		if (input.keyboard.s_key.is_down && !direction)
 		{
 			auto old_w = screen_width(state);
 			auto old_h = screen_height(state);
@@ -937,55 +727,21 @@ namespace app
 			state.render_new = true;
 		}
 
-		if (input.keyboard.right_key.is_down)
-		{
-			auto distance_per_pixel = screen_width(state) / BUFFER_WIDTH;
-
-			state.pixel_shift.x -= pixel_shift;
-			state.screen_pos.x += pixel_shift * distance_per_pixel;
-
-			state.render_new = true;
-		}
-
-		if (input.keyboard.left_key.is_down)
-		{
-			auto distance_per_pixel = screen_width(state) / BUFFER_WIDTH;
-
-			state.pixel_shift.x += pixel_shift;
-			state.screen_pos.x -= pixel_shift * distance_per_pixel;
-
-			state.render_new = true;
-		}
-
-		if (input.keyboard.up_key.is_down)
-		{
-			auto distance_per_pixel = screen_height(state) / BUFFER_HEIGHT;
-
-			state.pixel_shift.y += pixel_shift;
-			state.screen_pos.y -= pixel_shift * distance_per_pixel;
-
-			state.render_new = true;
-		}
-
-		if (input.keyboard.down_key.is_down)
-		{
-			auto distance_per_pixel = screen_height(state) / BUFFER_HEIGHT;
-
-			state.pixel_shift.y -= pixel_shift;
-			state.screen_pos.y += pixel_shift * distance_per_pixel;
-
-			state.render_new = true;
-		}
-
 		if (input.keyboard.plus_key.is_down)
 		{
-			state.max_iter = std::min(state.max_iter + 5, MAX_ITERATIONS_UPPER_LIMIT);
+			u32 adj = static_cast<u32>(0.02 * state.max_iter);
+			adj = std::max(adj, 5u);
+
+			state.max_iter = std::min(state.max_iter + adj, MAX_ITERATIONS_UPPER_LIMIT);
 			state.render_new = true;
 		}
 
 		if (input.keyboard.minus_key.is_down)
 		{
-			state.max_iter = std::max(state.max_iter - 5, MAX_ITERTAIONS_LOWER_LIMIT);
+			u32 adj = static_cast<u32>(0.02 * state.max_iter);
+			adj = std::max(adj, 5u);
+
+			state.max_iter = std::max(state.max_iter - adj, MAX_ITERTAIONS_LOWER_LIMIT);
 			state.render_new = true;
 		}
 	}
@@ -1016,16 +772,7 @@ namespace app
 		}
 
 		auto buffer_image = make_buffer_image(buffer);
-		copy(state.iterations, state.pixel_shift);
-		mandelbrot(state);
-
-		auto& mat = state.iterations;
-
-		auto [mat_min, mat_max] = std::minmax_element(mat.begin(), mat.end());
-		auto const min = *mat_min;
-		auto const max = *mat_max;
-
-		draw(buffer_image, mat, min, max);
+		render(buffer_image, state);
 
 		state.render_new = false;
 	}
