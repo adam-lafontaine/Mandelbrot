87881508083715870bed0a9ffcdbd1e36f1177cb
adam-lafontaine <adam.lafontaine@gmail.com>
2022-07-01 06:33:21 -0400

    memory buffer

--- src/cuda_app/device.cu
+++ src/cuda_app/device.cu
@@ -38,57 +38,124 @@ static void check_error(cudaError_t err, cstr label = "")
 
 namespace cuda
 {
-    bool device_malloc(void** addr, size_t n_bytes)
+    bool device_malloc(ByteBuffer& buffer, size_t n_bytes)
     {
         assert(n_bytes);
+        assert(!buffer.data);
 
-        if(!n_bytes)
+        if(!n_bytes || buffer.data)
         {
             return false;
         }
 
-        cudaError_t err = cudaMalloc(addr, n_bytes);
-        check_error(err, "device_malloc");
+        cudaError_t err = cudaMalloc((void**)&(buffer.data), n_bytes);
+        check_error(err, "malloc");
 
         bool result = err == cudaSuccess;
 
-        assert(result);
-
+        if(result)
+        {
+            buffer.capacity = n_bytes;
+        }
+        
         return result;
     }
 
 
-    bool unified_malloc(void** addr, size_t n_bytes)
+    bool unified_malloc(ByteBuffer& buffer, size_t n_bytes)
     {
         assert(n_bytes);
+        assert(!buffer.data);
 
-        if(!n_bytes)
+        if(!n_bytes || buffer.data)
         {
             return false;
         }
 
-        cudaError_t err = cudaMallocManaged(addr, n_bytes);
+        cudaError_t err = cudaMallocManaged((void**)&(buffer.data), n_bytes);
         check_error(err, "unified_malloc");
 
         bool result = err == cudaSuccess;
 
-        assert(result);
-
+        if(result)
+        {
+            buffer.capacity = n_bytes;
+        }
+        
         return result;
     }
 
 
-    bool free(void* data)
+    bool free(ByteBuffer& buffer)
     {
-        if(data)
+        buffer.capacity = 0;
+        buffer.size = 0;
+
+        if(buffer.data)
         {
+            cudaError_t err = cudaFree(buffer.data);
+            check_error(err, "free");
+
+            buffer.data = nullptr;
+
+            return err == cudaSuccess;
+        }
+
+        return true;
+    }
+
+
+    u8* push_bytes(ByteBuffer& buffer, size_t n_bytes)
+    {
+        assert(buffer.data);
+        assert(buffer.capacity);
+        assert(buffer.size < buffer.capacity);
+
+        auto is_valid = 
+            buffer.data &&
+            buffer.capacity &&
+            buffer.size < buffer.capacity;
+
+        auto bytes_available = (buffer.capacity - buffer.size) >= n_bytes;
+        assert(bytes_available);
+
+        if(!is_valid || !bytes_available)
+        {
+            return nullptr;
+        }
+
+        auto data = buffer.data + buffer.size;
+
+        buffer.size += n_bytes;
+        buffer.capacity -= n_bytes;
+
+        return data;
+    }
+
+
+    bool pop_bytes(ByteBuffer& buffer, size_t n_bytes)
+    {
+        assert(buffer.data);
+        assert(buffer.capacity);
+        assert(buffer.size <= buffer.capacity);
+        assert(n_bytes <= buffer.capacity);
+        assert(n_bytes <= buffer.size);
+
+        auto is_valid = 
+            buffer.data &&
+            buffer.capacity &&
+            buffer.size <= buffer.capacity &&
+            n_bytes <= buffer.capacity &&
+            n_bytes <= buffer.size;
+
+        if(is_valid)
+        {
+            buffer.size -= n_bytes;
+            buffer.capacity += n_bytes;
             return true;
         }
 
-        cudaError_t err = cudaFree(data);
-        check_error(err, "free");
-
-        return err == cudaSuccess;
+        return false;
     }
 
 
--- src/cuda_app/device.hpp
+++ src/cuda_app/device.hpp
@@ -5,14 +5,51 @@
 #include <cstddef>
 
 
+class ByteBuffer
+{
+public:
+    u8* data = nullptr;
+    size_t capacity = 0;
+    size_t size = 0;
+};
+
+
+template<typename T>
+class MemoryBuffer
+{
+public:
+    T* data = nullptr;
+    size_t capacity = 0;
+    size_t size = 0;    
+};
+
+
+template<typename T>
+ByteBuffer to_byte_buffer(MemoryBuffer<T> const& buffer)
+{
+    constexpr auto S = sizeof(T);
+
+    ByteBuffer b{};
+    b.data = (u8*)buffer.data;
+    b.capacity = S * buffer.capacity;
+    b.size = S * buffer.size;
+
+    return b;
+}
+
 
 namespace cuda
 {
-    bool device_malloc(void** addr, size_t n_bytes);
+    bool device_malloc(ByteBuffer& buffer, size_t n_bytes);
 
-    bool unified_malloc(void** addr, size_t n_bytes);
+    bool unified_malloc(ByteBuffer& buffer, size_t n_bytes);
 
-    bool free(void* data);
+    bool free(ByteBuffer& buffer);
+
+
+    u8* push_bytes(ByteBuffer& buffer, size_t n_bytes);
+
+    bool pop_bytes(ByteBuffer& buffer, size_t n_bytes);
 
 
     bool memcpy_to_device(const void* host_src, void* device_dst, size_t n_bytes);
@@ -23,7 +60,82 @@ namespace cuda
     bool no_errors(cstr label);
 
     bool launch_success(cstr label);
+
+
+    template<typename T>
+    bool device_malloc(MemoryBuffer<T>& buffer, size_t n_elements)
+    {
+        auto b = to_byte_buffer(buffer);
+        
+        if(!device_malloc(b, sizeof(T) * n_elements))
+        {
+            return false;
+        }
+
+        buffer.capacity = n_elements;
+
+        return true;
+    }
+
+
+    template<typename T>
+    bool unified_malloc(MemoryBuffer<T>& buffer, size_t n_elements)
+    {
+        auto b = to_byte_buffer(buffer);
+        
+        if(!unified_malloc(b, sizeof(T) * n_elements))
+        {
+            return false;
+        }
+
+        buffer.capacity = n_elements;
+
+        return true;
+    }
+
+
+    template<typename T>
+    bool free(MemoryBuffer<T>& buffer)
+    {
+        buffer.capacity = 0;
+        buffer.size = 0;
+
+        return free(to_byte_buffer(buffer));
+    }
+
+
+    template<typename T>
+    T* push_elements(MemoryBuffer<T>& buffer, size_t n_elements)
+    {
+        auto b = to_byte_buffer(buffer);
+
+        auto data = push_bytes(b, sizeof(T) * n_elements);
+
+        if(!data)
+        {
+            return nullptr;
+        }
+
+        buffer.capacity -= n_elements;
+        buffer.size += n_elements;
+
+        return (T*)data;
+    }
+
+
+    template<typename T>
+    bool pop_elements(MemoryBuffer<T>& buffer, size_t n_elements)
+    {
+        auto b = to_byte_buffer(buffer);
+
+        if(!pop_bytes(b, sizeof(T) * n_elements))
+        {
+            return false;
+        }
+
+        buffer.capacity += n_elements;
+        buffer.size -= n_elements;
+
+        return false;
+    }
 }
-
-
-#define device_addr(ptr) (void**)&(ptr)
