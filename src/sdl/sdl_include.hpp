f222de1fdfff1dbb853f89e8b37f4340da85f952
adam-lafontaine <adam.lafontaine@gmail.com>
2022-12-07 06:03:17 -0500

    windows icon

--- src/sdl/sdl_include.hpp
+++ src/sdl/sdl_include.hpp
@@ -185,14 +185,16 @@ static void close_game_controllers(SDLControllerInput& sdl, Input const& input)
 
 static void set_window_icon(SDL_Window* window)
 {
+#if _WIN32
+
     // this will "paste" the struct my_icon into this function
-#include "../resources/icon.h"
+#include "../resources/icon_64.h"
 
 // these masks are needed to tell SDL_CreateRGBSurface(From)
 // to assume the data it gets is byte-wise RGB(A) data
     Uint32 rmask, gmask, bmask, amask;
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-    int shift = (app_icon.bytes_per_pixel == 3) ? 8 : 0;
+    int shift = (window_icon.bytes_per_pixel == 3) ? 8 : 0;
     rmask = 0xff000000 >> shift;
     gmask = 0x00ff0000 >> shift;
     bmask = 0x0000ff00 >> shift;
@@ -201,20 +203,22 @@ static void set_window_icon(SDL_Window* window)
     rmask = 0x000000ff;
     gmask = 0x0000ff00;
     bmask = 0x00ff0000;
-    amask = (app_icon.bytes_per_pixel == 3) ? 0 : 0xff000000;
+    amask = (window_icon.bytes_per_pixel == 3) ? 0 : 0xff000000;
 #endif
 
     SDL_Surface* icon = SDL_CreateRGBSurfaceFrom(
-        (void*)app_icon.pixel_data,
-        app_icon.width, 
-        app_icon.height, 
-        app_icon.bytes_per_pixel * 8,
-        app_icon.bytes_per_pixel * app_icon.width, 
+        (void*)window_icon.pixel_data,
+        window_icon.width,
+        window_icon.height,
+        window_icon.bytes_per_pixel * 8,
+        window_icon.bytes_per_pixel * window_icon.width,
         rmask, gmask, bmask, amask);
 
     SDL_SetWindowIcon(window, icon);
 
     SDL_FreeSurface(icon);
+
+#endif // _WIN32
 }
 
 
